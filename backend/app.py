301add6ae9e9e5d428a1b8573a4e9b4231cfb6d5
Miranda  Gonzalez <mirandagc@MacBook-Pro-58.local>
2024-03-06 00:49:13 +0100

    Made changes necessary for dependencies for deployment, this is most updated version

--- backend/app.py
+++ backend/app.py
@@ -138,9 +138,9 @@ def user_details():
 
 
 #Initialize DB before the first request is processed
-@app.before_first_request
-def create_tables():
-    db.create_all()
+#@app.before_first_request
+#def create_tables():
+#    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
